cc8032c782f5fb1ecb02fd1ae5aa03006637f971
JoaoGuerini <joaopedrogp83@gmail.com>
2025-05-06 23:42:46 -0300

    Result para pesquisas

--- Guerini/index.js
+++ Guerini/index.js
@@ -1,11 +1,26 @@
 import express from 'express'
-import { padrao } from './servicos/consultas.js';
+import { GetReceitasByName, padrao } from './servicos/consultas.js'
 
 const app = express();
 
 app.get('/teste', async(req, res) =>{
     const teste = await padrao()
     res.json(teste)
+})
+
+app.get('/pesquisa', async(req, res) => {
+    const nome = req.query.nome;
+
+    if(typeof nome !== 'string')
+        return res.status(400).json({erro: "Teste"})
+
+    try{
+        const resultado = await GetReceitasByName(nome)
+        res.json(resultado)
+    }
+    catch{
+        res.status(500).json({erro: "Erro interno de servidor"})
+    }
 })
 
 app.listen(7777, () => {
--- Guerini/servicos/consultas.js
+++ Guerini/servicos/consultas.js
@@ -2,14 +2,45 @@ import pool from "./conexao.js";
 
 export async function padrao(){
     const conexao = await pool.getConnection()
-    const query = "SELECT * FROM Receita"
-    const res = executaQuery(conexao, query)
-    conexao.release()
-    return res;
+    try{
+        const query = "SELECT * FROM Receita"
+        const res = executaQuery(conexao, query)
+        return res;
+    }
+    catch (ex){
+        console.log(ex)
+    }
+    finally{
+        conexao.release()
+    }
 }
 
-async function executaQuery(conexao, query){
-    const resultado_query = await conexao.query(query)
-    const resposta = resultado_query[0]
-    return resposta
+export async function GetReceitasByName(name) {
+    if (typeof name !== 'string')
+        return
+
+    const conexao = await pool.getConnection()
+    try{
+        const query = 
+                    `SELECT r.nome, ir.imagem
+                    FROM Receita AS r
+                    INNER JOIN imagensReceitas AS ir ON r.id = ir.idReceita
+                    WHERE r.nome LIKE ?
+                    `;
+
+        const search = `%${name}%`
+        const res = executaQuery(conexao, query, [search])
+        return res;
+    }
+    catch (ex){
+        console.log(ex)
+    }
+    finally{
+        conexao.release()
+    }
+}
+
+async function executaQuery(conexao, query, params = []){
+    const [resultado] = await conexao.query(query, params)
+    return resultado
 }
