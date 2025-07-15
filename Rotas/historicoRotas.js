ed672f79e843859dca21e56691d54714c65a9d4b
Lopes <160147339+Lopes-Vh@users.noreply.github.com>
2025-07-14 20:49:34 -0400

    Update historicoRotas.js

--- Rotas/historicoRotas.js
+++ Rotas/historicoRotas.js
@@ -8,37 +8,49 @@ import {
 
 const historicoRouter = express.Router();
 
-// GET: Buscar histórico por ID do usuário (via query string)
-historicoRouter.get('/historico',
-  getHistorico
-  // #swagger.tags = ['Histórico']
-  // #swagger.description = 'Retorna o histórico de receitas visualizadas por um usuário específico'
-  // #swagger.parameters['idUsuario'] = { description: 'ID do usuário', in: 'query', required: true, type: 'integer' }
-);
+/* 
+  #swagger.tags = ['Histórico']
+  #swagger.description = 'Retorna o histórico de receitas visualizadas por um usuário específico'
+  #swagger.parameters['idUsuario'] = {
+    in: 'query',
+    description: 'ID do usuário',
+    required: true,
+    type: 'integer'
+  }
+*/
+historicoRouter.get('/historico', getHistorico);
 
-// POST: Adicionar item ao histórico
-historicoRouter.post('/historico',
-  postHistorico
-  // #swagger.tags = ['Histórico']
-  // #swagger.description = 'Adiciona uma nova visualização ao histórico do usuário'
-  // #swagger.parameters['body'] = {
-  //   in: 'body',
-  //   required: true,
-  //   schema: {
-  //     idUsuario: 1,
-  //     idReceita: 10,
-  //     nome: 'Bolo de Cenoura',
-  //     descricao: 'Delicioso bolo com cobertura de chocolate.'
-  //   }
-  // }
-);
+/* 
+  #swagger.tags = ['Histórico']
+  #swagger.description = 'Adiciona uma nova visualização ao histórico do usuário'
+  #swagger.requestBody = {
+    required: true,
+    content: {
+      "application/json": {
+        schema: {
+          type: 'object',
+          required: ['idUsuario', 'idReceita'],
+          properties: {
+            idUsuario: { type: 'integer', example: 1 },
+            idReceita: { type: 'integer', example: 10 }
+          }
+        }
+      }
+    }
+  }
+*/
+historicoRouter.post('/historico', postHistorico);
 
-// DELETE: Limpar histórico de um usuário pelo ID
-historicoRouter.delete('/historico/:idUsuario',
-  deleteHistorico
-  // #swagger.tags = ['Histórico']
-  // #swagger.description = 'Remove todos os registros de histórico do usuário informado'
-  // #swagger.parameters['idUsuario'] = { description: 'ID do usuário', in: 'path', required: true, type: 'integer' }
-);
+/* 
+  #swagger.tags = ['Histórico']
+  #swagger.description = 'Remove todos os registros de histórico do usuário informado'
+  #swagger.parameters['idUsuario'] = {
+    in: 'path',
+    description: 'ID do usuário',
+    required: true,
+    type: 'integer'
+  }
+*/
+historicoRouter.delete('/historico/:idUsuario', deleteHistorico);
 
 export default historicoRouter;
